69880d12b6281bcb1f706321fc784fea169a842b
Clayton Pereira <cpereiramt@gmail.com>
2020-08-11 19:10:34 -0400

    Saving changes on Main page

---
+++ src/Pages/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <div>
+            <div id="body-div">
+  
+            </div>
+        </div>
+    )
+}
+
+export default Main
