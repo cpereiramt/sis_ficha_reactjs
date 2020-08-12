1717f4db6259a5f178908659cee98e041487bdcd
Clayton Pereira <cpereiramt@gmail.com>
2020-08-11 22:00:18 -0400

    Saving tabledata

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Main.css';
 import TopBar from '../components/TopBar';
 import SearchBar from '../components/SearchBar';
+import TableData from '../components/TableData';
 
 function Main() {
     return (
@@ -9,6 +10,7 @@ function Main() {
             <div id="body-div">
             <TopBar />
             < SearchBar />
+            <TableData />
             </div>
         </div>
     )
