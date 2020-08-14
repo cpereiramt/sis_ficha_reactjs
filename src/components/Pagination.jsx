ed3e372acd331c1eb63ba6ce1e11cc7487bec278
Clayton Pereira <cpereiramt@gmail.com>
2020-08-14 16:05:56 -0400

    Saving changes in fetch

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -5,9 +5,9 @@ function Pagination(props) {
     const letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
     const handleClick = (event) => {
-        const allButtons = document.getElementById('pagination_div');               
-        const teste =   Array.from(allButtons.childNodes); 
-        teste.map(element => element.removeAttribute('id', 'selectedButton'))
+        const allButtons = document.getElementById('pagination_div');              
+        const buttonActive = Array.from(allButtons.childNodes); 
+        buttonActive.map(element => element.removeAttribute('id', 'selectedButton'))
         const selectedButton =  document.getElementsByName(event.target.name);
         selectedButton[0].setAttribute('id','selectedButton');
         console.log(props.function(selectedButton[0].name));
@@ -16,7 +16,7 @@ function Pagination(props) {
     return (
     
         <div id="pagination_div">
-            {letters.map((buttonE,i) => <button name={buttonE} onClick={(event) => handleClick(event)}> 
+            {letters.map((buttonE,i) => <button name={buttonE}  onClick={(event) => handleClick(event)}> 
                 {buttonE} 
                 </button>)}
        </div>
