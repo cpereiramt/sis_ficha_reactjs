9e63c5f87105b8ab5b324638c29d38a548f17c0b
Clayton Pereira <cpereiramt@gmail.com>
2020-08-25 18:35:57 -0400

    Saving changes in project

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,22 +2,38 @@ import React, { useEffect,useState } from 'react';
 import brasao from '../images/brasao_estado_cor.png'
 import './Home.css';
 import {navigate} from 'hookrouter';
+import { useAuth0 } from "@auth0/auth0-react";
 
-function Home() {
-    const [page, setPage ] = useState(0);
+
+function Home(props) {
+/*     const [page, setPage ] = useState(0);
     function redirect(){
         navigate('/TelaPrincipal',true)
     }
     useEffect(() => {
         setPage(1);
-    }, [])
+    }, []) */
+    const { loginWithRedirect } = useAuth0();
+    const { user, isAuthenticated } = useAuth0();
+    const [userLogged, setUserLogged ] = useState();
+    const [isAuth, setIsAuth] = useState();
+
+
+    useEffect(() => {
+        setUserLogged(user);
+        setIsAuth(isAuthenticated);
+    }, [isAuthenticated])
+
     return (
         <div id="home-body">
-           <img id="brasao_inicio" src={brasao} alt=""/>
+           { isAuth 
+           ? navigate('/TelaPrincipal',false)
+                     :<> <img id="brasao_inicio" src={brasao} alt=""/>
            <div id="div_body">
            <h1 id="Home_h1">Tela Inicial Sisficha !</h1>
-           <button id="Home_button" onClick={() => redirect()}>Login With Google</button>
-           </div>
+           <button id="Home_button" onClick={() => loginWithRedirect()}>Login</button>
+           </div> </> }
+           
         </div>
     )
 }
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import userPhoto from '../images/imageTopProfile.png';
 import SearchBar from '../components/SearchBar';
+import { useAuth0 } from "@auth0/auth0-react";
 
 import './TopBar.css';
 
-function TopBar() {
+function TopBar(props) {
+  const { logout  } = useAuth0();
+
+
     return (
         <div id="topbar-body">
        < SearchBar />
+       {console.log(props.user)}
         <div id="topbar-section">
-          <img id="userPhoto" src={userPhoto} alt=""/> 
-          <h2 id="userName"> Username </h2>
+          <img id="userPhoto" src={props.user.picture}  alt=""/> 
+          <h2 id="userName"> {props.user.name} </h2>
+          <button onClick={() => logout({ returnTo: window.location.origin })}>Sair </button>
         </div>
         </div>
  
