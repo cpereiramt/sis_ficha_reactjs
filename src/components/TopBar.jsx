54b521118861d3fb46c2850a3beaa76415a8c450
Clayton Guimaraes Leite Pereira <claytonpereira@mtprev.local>
2020-08-27 08:16:41 -0400

    Removing ouath

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,22 +1,19 @@
 import React, {useState, useEffect} from 'react'
 import userPhoto from '../images/imageTopProfile.png';
 import SearchBar from '../components/SearchBar';
-import { useAuth0 } from "@auth0/auth0-react";
+import {navigate } from 'hookrouter';
 
 import './TopBar.css';
 
 function TopBar(props) {
-  const { logout  } = useAuth0();
-
 
     return (
         <div id="topbar-body">
        < SearchBar />
-       {console.log(props.user)}
         <div id="topbar-section">
-          <img id="userPhoto" src={props.user.picture}  alt=""/> 
-          <h2 id="userName"> {props.user.name} </h2>
-          <button id="logout_button" onClick={() => logout({ returnTo: window.location.origin })}>Sair </button>
+          <img id="userPhoto" src={userPhoto}  alt=""/> 
+          <h2 id="userName"> UserName </h2>
+          <button onClick={() => navigate('/TelaPrincipal')}>Sair </button>
         </div>
         </div>
  
